51951a49a3a18494fa03ff29d6611bbdbb4fd1cc
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-19 20:06:11 -0600

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AskQuestion = () => {
+  return <div>Ask a Question</div>;
+};
+
+export default AskQuestion;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJs14 We&apos;re coming</h1>
-      <h2 className="h2-bold">NextJs14 We&apos;re coming</h2>
-      <h3 className="h3-bold">NextJs14 We&apos;re coming</h3>
-    </div>
-  );
-}
