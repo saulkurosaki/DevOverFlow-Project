904a8f4fa333d7e7aa800e45aae008469cfe6b56
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-23 21:39:02 -0600

    implement formatAndDivideNumber and getTimeStamp utils functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,47 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diffInMilliseconds = now.getTime() - createdAt.getTime();
+
+  const seconds = Math.floor(diffInMilliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years > 0) {
+    return `${years} ${years === 1 ? "year" : "years"} ago`;
+  } else if (months > 0) {
+    return `${months} ${months === 1 ? "month" : "months"} ago`;
+  } else if (weeks > 0) {
+    return `${weeks} ${weeks === 1 ? "week" : "weeks"} ago`;
+  } else if (days > 0) {
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  } else if (hours > 0) {
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  } else {
+    return `${seconds} ${seconds === 1 ? "second" : "seconds"} ago`;
+  }
+};
+
+export const formatAndDivideNumber = (num: number): string => {
+  if (num >= 1000000) {
+    const formattedNum = (num / 1000000).toFixed(1);
+    return `${formattedNum}M`;
+  } else if (num >= 1000) {
+    const formattedNum = (num / 1000).toFixed(1);
+    return `${formattedNum}K`;
+  } else {
+    return num.toString();
+  }
+};
