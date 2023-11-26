7250016200dc866515900ed6db5edcb9d8130c7d
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-25 18:17:05 -0600

    implement final display-questions in the homepage feature

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,38 +5,12 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: 1,
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [
-      { _id: 1, name: "python" },
-      { _id: 2, name: "sql" },
-    ],
-    author: { _id: 101, name: "John Doe", picture: "john-doe.jpg" },
-    upvotes: 1500000,
-    views: 100,
-    answers: [{}, {}], // Array of objects as specified in the interface
-    createdAt: new Date("2023-11-20T12:00:00.000Z"),
-  },
-  {
-    _id: 2,
-    title: "How do I use Express as a custom server in NextJS",
-    tags: [
-      { _id: 1, name: "next13" },
-      { _id: 2, name: "express" },
-    ],
-    author: { _id: 102, name: "John Doe", picture: "john-doe.jpg" },
-    upvotes: 512400,
-    views: 100,
-    answers: [{}, {}], // Array of objects as specified in the interface
-    createdAt: new Date("2022-11-23T12:00:00.000Z"),
-  },
-];
+export default async function Home() {
+  const result = await getQuestions({});
 
-export default function Home() {
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -67,8 +41,8 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
