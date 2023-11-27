b741f8e673053c86877fce166d1cace2ef0b73b7
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-27 11:48:12 -0600

    route.ts update

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -53,6 +53,8 @@ export async function POST(req: Request) {
 
   const eventType = evt.type;
 
+  console.log({ eventType });
+
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
@@ -98,5 +100,5 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
 
-  return NextResponse.json({ message: "OK" });
+  return new Response("", { status: 201 });
 }
