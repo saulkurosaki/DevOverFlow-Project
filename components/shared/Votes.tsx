3fed6aa9cb3ac9855452f063f68486e9ee1306df
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-29 11:34:36 -0600

    implement votes.tsx complete component without handle-save and handle-vote functions

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,94 @@
+"use client";
+
+import { formatAndDivideNumber } from "@/lib/utils";
+import Image from "next/image";
 import React from "react";
 
-const Votes = () => {
-  return <div>Votes Testc:</div>;
+interface Params {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Params) => {
+  const handleSave = () => {};
+
+  const handleVote = (action: string) => {};
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-2.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            alt="upvote"
+            width={18}
+            height={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-2.5">
+          <Image
+            src={
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            alt="downvote"
+            width={18}
+            height={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Image
+        src={
+          hasSaved
+            ? "/assets/icons/star-filled.svg"
+            : "/assets/icons/star-red.svg"
+        }
+        alt="save"
+        width={18}
+        height={18}
+        className="cursor-pointer"
+        onClick={handleSave}
+      />
+    </div>
+  );
 };
 
 export default Votes;
