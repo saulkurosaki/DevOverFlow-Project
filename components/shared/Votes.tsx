783ac3f23e4ab42d52cfe6f7dc3f43e48e86c04a
Saul Paredes <saulkurosaki13@gmail.com>
2023-11-29 14:26:01 -0600

    implement handle-save function

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -5,6 +5,7 @@ import {
   downvoteQuestion,
   upvoteQuestion,
 } from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
@@ -32,9 +33,15 @@ const Votes = ({
   hasSaved,
 }: Params) => {
   const pathname = usePathname();
-  const router = useRouter();
+  //   const router = useRouter();
 
-  const handleSave = () => {};
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      userId: JSON.parse(userId),
+      questionId: JSON.parse(itemId),
+      path: pathname,
+    });
+  };
 
   const handleVote = async (action: string) => {
     if (!userId) return;
