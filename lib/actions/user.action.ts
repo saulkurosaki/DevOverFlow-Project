1b84abaae3bdf80964576a3c00fec78158f5b0f8
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-04 12:02:24 -0600

    update get-saved-question server action for filtering

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -190,11 +190,38 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       ];
     }
 
+    let sortOptions = {};
+
+    switch (filter) {
+      case "most_recent":
+        sortOptions = { createdAt: -1 };
+        break;
+
+      case "oldest":
+        sortOptions = { createdAt: 1 };
+        break;
+
+      case "most_voted":
+        sortOptions = { upvotes: -1 };
+        break;
+
+      case "most_viewed":
+        sortOptions = { views: -1 };
+        break;
+
+      case "most_answered":
+        sortOptions = { answers: -1 };
+        break;
+
+      default:
+        break;
+    }
+
     const user = await User.findOne(query).populate({
       path: "saved",
       match: query,
       options: {
-        sort: { createdAt: -1 },
+        sort: sortOptions,
       },
       populate: [
         { path: "tags", model: Tag, select: "_id name" },
