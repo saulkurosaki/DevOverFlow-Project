8b58177bef27f063db361015259eaed8335d91ab
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-03 20:44:49 -0600

    implement tags page local-searchbar feature

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -4,9 +4,12 @@ import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
 import { getAllTags } from "@/lib/actions/tag.actions";
 import Link from "next/link";
+import { SearchParamsProps } from "@/types";
 
-const Tags = async () => {
-  const result = await getAllTags({});
+const Tags = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getAllTags({
+    searchQuery: searchParams.q,
+  });
 
   return (
     <>
