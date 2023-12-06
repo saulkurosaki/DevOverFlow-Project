e0e7966d4922805232507f4b3ed1e4022df84980
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-06 07:21:01 -0600

    implemenet complete website global-search feature

--- components/shared/search/GlobalSearch.tsx
+++ components/shared/search/GlobalSearch.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
@@ -11,11 +11,33 @@ const GlobalSearch = () => {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const searchContainerRef = useRef(null);
 
   const query = searchParams.get("q");
 
   const [search, setSearch] = useState(query || "");
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: any) => {
+      if (
+        searchContainerRef.current &&
+        // @ts-ignore
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+        setSearch("");
+      }
+    };
+
+    setIsOpen(false);
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [pathname]);
 
   useEffect(() => {
     const delayDebounceFn = setTimeout(() => {
@@ -40,14 +62,17 @@ const GlobalSearch = () => {
     }, 300);
 
     return () => clearTimeout(delayDebounceFn);
-  }, [search, pathname, router, searchParams, query]);
+  }, [search, router, pathname, searchParams, query]);
 
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div
+      className="relative w-full max-w-[600px] max-lg:hidden"
+      ref={searchContainerRef}
+    >
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
-          alt="Search"
+          alt="search"
           width={24}
           height={24}
           className="cursor-pointer"
@@ -55,7 +80,7 @@ const GlobalSearch = () => {
 
         <Input
           type="text"
-          placeholder="Search anything globally..."
+          placeholder="Search globally"
           value={search}
           onChange={(e) => {
             setSearch(e.target.value);
@@ -66,7 +91,6 @@ const GlobalSearch = () => {
           className="paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
-
       {isOpen && <GlobalResult />}
     </div>
   );
