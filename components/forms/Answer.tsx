89414220e74e646f0b235adbb14569299b1614e5
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-06 13:11:46 -0600

    implement generate-ai-answer function

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -28,6 +28,7 @@ interface Props {
 const Answer = ({ question, questionId, authorId }: Props) => {
   const pathname = usePathname();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmittingAi, setIsSubmittingAi] = useState(false);
   const { mode } = useTheme();
   const editorRef = useRef(null);
 
@@ -63,6 +64,31 @@ const Answer = ({ question, questionId, authorId }: Props) => {
     }
   };
 
+  const generateAiAnswer = async () => {
+    if (!authorId) return;
+
+    setIsSubmittingAi(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+        {
+          method: "POST",
+          body: JSON.stringify({ question }),
+        }
+      );
+
+      const aiAnswer = await response.json();
+
+      alert(aiAnswer.reply);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsSubmittingAi(false);
+    }
+  };
+
   return (
     <div>
       <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -71,7 +97,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
         </h4>
 
         <Button
-          onClick={() => {}}
+          onClick={generateAiAnswer}
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
         >
           <Image
@@ -81,7 +107,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             height={12}
             className="object-contain"
           />
-          Generate an AI Answer
+          Generate AI Answer
         </Button>
       </div>
 
