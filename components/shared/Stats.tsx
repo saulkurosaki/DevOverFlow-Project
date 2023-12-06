5fddb35d96b9e6ac8b54802b184ea84c471b05a8
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-06 08:50:45 -0600

    update stats component for badges system

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,6 +1,6 @@
 import { formatAndDivideNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types";
 import Image from "next/image";
-import React from "react";
 
 interface StatsCardProps {
   imgUrl: string;
@@ -23,14 +23,18 @@ const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
+  badges: BadgeCounts;
+  reputation: number;
 }
 
-const Stats = ({ totalQuestions, totalAnswers }: Props) => {
+const Stats = ({ totalQuestions, totalAnswers, badges, reputation }: Props) => {
   return (
     <div className="mt-10">
-      <h4 className="h3-semibold text-dark200_light900">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Reputation - {reputation}
+      </h4>
 
-      <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 sm:grid-cols-4">
+      <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
           <div>
             <p className="paragraph-semibold text-dark200_light900">
@@ -38,7 +42,6 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
             </p>
             <p className="body-medium text-dark400_light700">Questions</p>
           </div>
-
           <div>
             <p className="paragraph-semibold text-dark200_light900">
               {formatAndDivideNumber(totalAnswers)}
@@ -49,17 +52,19 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
 
         <StatsCard
           imgUrl="/assets/icons/gold-medal.svg"
-          value={0}
+          value={badges.GOLD}
           title="Gold Badges"
         />
+
         <StatsCard
           imgUrl="/assets/icons/silver-medal.svg"
-          value={0}
+          value={badges.SILVER}
           title="Silver Badges"
         />
+
         <StatsCard
           imgUrl="/assets/icons/bronze-medal.svg"
-          value={0}
+          value={badges.BRONZE}
           title="Bronze Badges"
         />
       </div>
