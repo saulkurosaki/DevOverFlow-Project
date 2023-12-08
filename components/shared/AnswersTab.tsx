de7c2595e7b9f6c406ce79ac4c87b596c1f6782b
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-07 20:14:13 -0600

    fix bugs for deployment

--- components/shared/AnswersTab.tsx
+++ components/shared/AnswersTab.tsx
@@ -31,7 +31,7 @@ const AnswersTab = async ({ searchParams, userId, clerkId }: Props) => {
       <div className="mt-10">
         <Pagination
           pageNumber={searchParams?.page ? +searchParams.page : 1}
-          isNext={result.isNext}
+          isNext={result.isNextAnswer}
         />
       </div>
     </>
